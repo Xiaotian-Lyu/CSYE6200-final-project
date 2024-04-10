6f47d121358ebdb77c1bfe291daea1efa1b64f39
danni-nu <zhou.dann@northeastern.edu>
2024-04-10 01:06:01 -0400

    First Scene

---
+++ forestadventure/src/cc/forestadventure/scene/GameScene.java
@@ -0,0 +1,35 @@
+package cc.forestadventure.scene;
+
+import cc.forestadventure.Director;
+import javafx.animation.AnimationTimer;
+import javafx.event.EventHandler;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.input.KeyEvent;
+
+public class GameScene {
+	private Canvas canvas = new Canvas(Director.WIDTH, Director.HEIGHT);
+	private GraphicsContext graphicsContext = canvas.getGraphicsContext2D();
+	
+	private KeyProcess keyProcess = new KeyProcess();
+	private Refresh refresh = new Refresh();
+	
+	private void paint() {
+		
+	}
+	
+	private class Refresh extends AnimationTimer {
+		public void handle(long now) {
+			paint();
+		}
+	}
+	
+	private class KeyProcess implements EventHandler<KeyEvent>{
+		@Override
+		public void handle(KeyEvent event) {
+		
+	}
+	}
+		
+
+}
